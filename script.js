266976de584cdc4b974532855b734ecf011d77b9
KonulAzizova <knl.seyidova.88@mail.ru>
2023-08-28 12:34:35 +0400

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let loggedEmail = document.querySelector('.logged_container p');
 let logOut = document.querySelector('.logged_container svg');
 let getloggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-let bestComediesContainer=document.querySelector('#best_comedies_container');
+let bestComediesContainer = document.querySelector('#best_comedies_container');
 let sliderContainer = document.querySelector('#popular_move_container');
 let videosContainer = document.querySelector('.videos_container');
 let leftSliderIcon = document.querySelector('.left_slider_icon');
@@ -13,13 +13,18 @@ let rightSliderIcon2 = document.querySelector('.right_slider_icon2');
 let actionAdventureContainer = document.querySelector('#action_adventure_container');
 
 
-logOut.addEventListener('click',()=>{
-    window.location.href='./entrance.html'
+let barsIcon = document.querySelector('.bars_icon');
+let navList = document.querySelector('.nav_list');
+let closeIcon = document.querySelector('.close_icon');
+
+
+logOut.addEventListener('click', () => {
+    window.location.href = './entrance.html'
     localStorage.removeItem("loggedInUser")
 })
 
-loggedEmail.innerHTML =`${getloggedInUser ?getloggedInUser.email :""}`;
- 
+loggedEmail.innerHTML = `${getloggedInUser ? getloggedInUser.email : ""}`;
+
 
 fetch('./api/videos.json')
     .then(resp => resp.json())
@@ -32,11 +37,9 @@ fetch('./api/videos.json')
  <h3>${x.title}</h3>
 </div>
  
- 
- 
- 
-
  `
+
+
 
         })
     })
@@ -46,12 +49,15 @@ let count1 = 0;
 let count2 = 0;
 
 
+
 function slider() {
     for (let i = 0; i < sliderContainer.children.length; i++) {
         sliderContainer.children[i].style.transform = `translateX(${-302 * count}px)`
     }
 
 }
+
+
 function slider1() {
     for (let i = 0; i < bestComediesContainer.children.length; i++) {
         bestComediesContainer.children[i].style.transform = `translateX(${-302 * count1}px)`
@@ -81,31 +87,32 @@ setInterval(() => {
     }
 }, 3000);
 
+
 setInterval(() => {
-    if(count1<bestComediesContainer.children.length-4){
+    if (count1 < bestComediesContainer.children.length - 4) {
         count1++
         slider1()
-    }else{
-        count1=0
+    } else {
+        count1 = 0
         slider1()
     }
-    
+
 }, 3000);
 
 setInterval(() => {
-    if(count2<actionAdventureContainer.children.length-4){
+    if (count2 < actionAdventureContainer.children.length - 4) {
         count2++
         slider2()
-    }else{
-        count2=0
+    } else {
+        count2 = 0
         slider2()
     }
-    
+
 }, 3000);
 
 
 rightSliderIcon.addEventListener('click', () => {
-  if (count < sliderContainer.children.length - 4) {
+    if (count < sliderContainer.children.length - 4) {
         count++
         slider()
     } else {
@@ -114,52 +121,59 @@ rightSliderIcon.addEventListener('click', () => {
     }
 
 })
-leftSliderIcon.addEventListener('.left_slider_icon',()=>{
-    if(count>0){
+leftSliderIcon.addEventListener('.left_slider_icon', () => {
+    if (count > 0) {
         count--
         slider()
-    }else{
-        count=0
+    } else {
+        count = 0
         slider()
     }
 })
 
 rightSliderIcon1.addEventListener('click', () => {
     if (count1 < bestComediesContainer.children.length - 4) {
-          count1++
-          slider1()
-      } else {
-          count1 = 0
-          slider1()
-      }
-  
-  })
-  leftSliderIcon1.addEventListener('click',()=>{
-      if(count1>0){
-          count1--
-          slider1()
-      }else{
-          count1=0
-          slider1()
-      }
-  })
+        count1++
+        slider1()
+    } else {
+        count1 = 0
+        slider1()
+    }
 
-  rightSliderIcon2.addEventListener('click', () => {
+})
+leftSliderIcon1.addEventListener('click', () => {
+    if (count1 > 0) {
+        count1--
+        slider1()
+    } else {
+        count1 = 0
+        slider1()
+    }
+})
+
+rightSliderIcon2.addEventListener('click', () => {
     if (count2 < actionAdventureContainer.children.length - 4) {
-          count2++
-          slider2()
-      } else {
-          count2 = 0
-          slider2()
-      }
-  
-  })
-  leftSliderIcon2.addEventListener('click',()=>{
-      if(count2>0){
-          count2--
-          slider2()
-      }else{
-          count2=0
-          slider2()
-      }
-  })
+        count2++
+        slider2()
+    } else {
+        count2 = 0
+        slider2()
+    }
+
+})
+leftSliderIcon2.addEventListener('click', () => {
+    if (count2 > 0) {
+        count2--
+        slider2()
+    } else {
+        count2 = 0
+        slider2()
+    }
+})
+
+barsIcon.addEventListener('click', () => {
+    navList.classList.add('nav_active')
+})
+closeIcon.addEventListener('click', () => {
+    navList.classList.remove('nav_active')
+})
---
+++ script2.js
@@ -0,0 +1,47 @@
+
+
+
+let myList = document.querySelector('#my_list');
+let leftSliderIcon2 = document.querySelector('.left_slider_icon5');
+let rightSliderIcon2 = document.querySelector('.right_slider_icon5');
+
+let count3 = 0;
+function slider5() {
+    for (let i = 0; i < myList.children.length; i++) {
+        myList.children[i].style.transform = `translateX(${-302 * count3}px)`
+    }
+
+}
+
+
+setInterval(() => {
+    if (count3 < myList.children.length - 4) {
+        count3++
+        slider5()
+    } else {
+        count3 = 0
+        slider5()
+    }
+}, 3000);
+
+
+
+rightSliderIcon2.addEventListener('click', () => {
+    if (count3 < myList.children.length - 4) {
+        count3++
+        slider5()
+    } else {
+        count3 = 0
+        slider5()
+    }
+
+})
+leftSliderIcon2.addEventListener('click', () => {
+    if (count3 > 0) {
+        count3--
+        slider5()
+    } else {
+        count3 = 0
+        slider5()
+    }
+})
